d8e7d01e7e3018f5487c518ecd732d2b99ba3ee6
Tom Burns <tom@burns.ca>
2022-06-22 11:05:55 -0400

    need to ignore here

--- src/plugins/drumsampler/src/views/DrumSamplerView.tsx
+++ src/plugins/drumsampler/src/views/DrumSamplerView.tsx
@@ -9,6 +9,7 @@ import { DrumSamplerVoiceState } from '../Voice';
 import {WaveformView} from "./WaveformView"
 import { WamAsset } from 'wam-extensions';
 
+// @ts-ignore
 let styles = styleRoot.locals as typeof styleRoot
 
 const c = (a: string[]) => a.filter(el => !!el).join(" ")
