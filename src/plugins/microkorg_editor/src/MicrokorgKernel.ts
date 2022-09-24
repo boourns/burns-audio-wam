ee8c83695a355810aefa4ab1faefb53af13f700b
Tom Burns <tom@burns.ca>
2022-09-24 11:50:04 -0400

    sysex packing / unpacking

--- src/plugins/microkorg_editor/src/MicrokorgKernel.ts
+++ src/plugins/microkorg_editor/src/MicrokorgKernel.ts
@@ -607,7 +607,7 @@ export class MicrokorgKernel implements MIDIControllerKernel {
             return false
         }
 
-        let sysex = this.unpackKorg(data, 5)
+        let sysex = this.unpackKorg(data, 5, data.length-1)
 
         return false
     }
@@ -622,19 +622,45 @@ export class MicrokorgKernel implements MIDIControllerKernel {
         return results
     }
 
-    unpackKorg(packed: Uint8Array, startIndex: number): number[] {
+    unpackKorg(packed: Uint8Array, startIndex: number, endIndex: number = packed.length): number[] {
         let result: number[] = []
 
         let i
-        for (i = startIndex; i < packed.length; i += 8) {
+        for (i = startIndex; i < endIndex; i += 8) {
 
             for (let j = 0; j < 7; j++) {
                 const topBit = ((packed[i] & (0x1 << j)) != 0) ? 0x80 : 0
                 
-                if (j + i + 2 < packed.length) {
+                if (j + i + 1 < endIndex) {
                     result.push(packed[j+i+1] | topBit)
                 }
             }
+        }
+
+        return result
+    }
+
+    packKorg(unpacked: number[]): number[] {
+        let result: number[] = []
+
+        let i
+        for (i = 0; i < unpacked.length; i += 7) {
+            let dataSet: number[] = []
+            let topbitByte = 0
+
+            for (let j = 0; j < 7; j++) {
+                if (i+j < unpacked.length) {
+                    let incoming = unpacked[i + j]
+                    if ((incoming & 0x80) != 0) {
+                        topbitByte |= 0x1 << j
+                        incoming &= 0x7f
+                    }
+                    dataSet.push(incoming)
+                }
+            }
+
+            result.push(topbitByte)
+            result.push(...dataSet)
         }
 
         return result
--- src/plugins/microkorg_editor/test/MicrokorgKernelTest.ts
+++ src/plugins/microkorg_editor/test/MicrokorgKernelTest.ts
@@ -5,13 +5,34 @@ import { ControlChangeMessager } from "../src/ControlChangeMessager.js"
 import {MicrokorgKernel} from "../src/MicrokorgKernel.js"
 
 describe("MicrokorgKernel", () => {
+    it("packKorg packs bytes", async() => {
+        const input = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f, 0xff, 0xff, 0xff]
+        const kernel = new MicrokorgKernel()
+        const packed = kernel.packKorg(input)
+
+        deepStrictEqual(packed, [63, 127, 127, 127,
+            127, 127, 127, 127,
+              7, 127, 127, 127])
+    })
+
+    it("unpackKorg unpacks sysex bytes", async () => {
+        const input = [63, 127, 127, 127,
+            127, 127, 127, 127,
+              7, 127, 127, 127]
+
+        const kernel = new MicrokorgKernel()
+
+        const unpacked = kernel.unpackKorg(new Uint8Array(input), 0)
+
+        deepStrictEqual([0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f, 0xff, 0xff, 0xff], unpacked)
+
+    })
+
     it("Can load sysex", async () => {
         const patch = new Uint8Array([240, 66, 48, 88, 64, 0, 32, 32, 32, 32, 32, 32, 32, 0, 32, 32, 32, 32, 32, 0, 0, 32, 7, 0, 64, 0, 60, 7, 40, 0, 85, 2, 20, 30, 0, 16, 76, 0, 15, 61, 0, 120, 0, 0, 80, 8, 1, 0, 0, 127, 112, 10, 64, 0, 66, 64, 65, 3, 15, 0, 0, 0, 0, 2, 64, 73, 0, 127, 100, 0, 3, 1, 57, 30, 70, 64, 64, 0, 127, 64, 0, 64, 64, 0, 64, 0, 0, 95, 2, 64, 127, 114, 2, 0, 10, 3, 2, 70, 12, 2, 71, 0, 71, 54, 3, 66, 67, 64, 0, 0, 0, 0, 0, 64, 64, 64, 64, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 64, 64, 64, 64, 0, 0, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 64, 0, 0, 64, 64, 64, 0, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 127, 0, 112, 10, 64, 66, 64, 69, 0, 0, 0, 0, 0, 0, 0, 64, 64, 0, 0, 127, 0, 0, 1, 127, 20, 0, 64, 64, 64, 127, 64, 0, 64, 0, 64, 0, 64, 127, 0, 0, 64, 0, 127, 0, 2, 10, 3, 2, 70, 0, 12, 2, 64, 3, 64, 66, 64, 0, 67, 64, 0, 0, 0, 0, 64, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 0, 0, 64, 64, 64, 64, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 64, 64, 64, 64, 0, 0, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 64, 64, 64, 64, 64, 64, 0, 64, 64, 247])
 
         const kernel = new MicrokorgKernel()
         let result = kernel.fromSysex(patch)
-
-        console.log("Result is ", result)
     })
 
 })
