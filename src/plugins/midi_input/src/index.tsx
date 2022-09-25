780f9579b495b2eb80b48563b196abaeeb860c65
Tom Burns <tom@burns.ca>
2022-09-25 19:08:34 -0400

    save midi input port choice

--- src/plugins/midi_input/src/index.tsx
+++ src/plugins/midi_input/src/index.tsx
@@ -20,8 +20,6 @@ export default class MIDIInputModule extends WebAudioModule<MIDIInputNode> {
 	_processorUrl = `${this._baseURL}/MIDIInputProcessor.js`;
 	nonce: string | undefined;
 
-	callback?: () => void
-	midiInitialized: boolean = false
 
 	async _loadDescriptor() {
 		const url = this._descriptorUrl;
@@ -48,43 +46,11 @@ export default class MIDIInputModule extends WebAudioModule<MIDIInputNode> {
 
 		this.audioNode = node
 
-		try {
-			let midi = await navigator.requestMIDIAccess({sysex: true});
-			midi.addEventListener('statechange', (event: WebMidi.MIDIConnectionEvent) => this.midiReady(event.target as WebMidi.MIDIAccess));
-
-			this.midiReady(midi)
-		} catch (err) {
-			console.log('Error accessing MIDI devices: ', err);
-		}	  
+		await node.initializeMidi()
 	  
 		if (initialState) node.setState(initialState);
 		return node;
     }
-
-	midiReady(midi: WebMidi.MIDIAccess) {
-		// Reset.
-		this.audioNode.midiIn = [];
-		this.audioNode.midiOut = [];
-		this.audioNode.selectedDevice = -1;
-		
-		// MIDI devices that send you data.
-		const inputs = midi.inputs.values();
-		for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
-			this.audioNode.midiIn.push(input.value);
-		}
-		
-		// MIDI devices that you send data to.
-		const outputs = midi.outputs.values();
-		for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
-			this.audioNode.midiOut.push(output.value);
-		}
-
-		this.midiInitialized = true
-
-		if (this.callback) {
-			this.callback()
-		}
-	}
 
 	async createGui() {
 		const div = document.createElement('div');
@@ -111,7 +77,7 @@ export default class MIDIInputModule extends WebAudioModule<MIDIInputNode> {
 		// @ts-ignore
 		styleRoot.use({ target: shadow });
 
-		render(<MIDIInputView plugin={this}></MIDIInputView>, shadow);
+		render(<MIDIInputView plugin={this.audioNode}></MIDIInputView>, shadow);
 
 		return div;
 	}
