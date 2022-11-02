cc584c7915d4a52ebb89c085fba0009a6cbbfe96
Tom Burns <tom@burns.ca>
2022-11-02 10:37:07 -0400

    example RNBO WAM

---
+++ src/plugins/rnbo/src/index.tsx
@@ -0,0 +1,93 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-len */
+/* eslint-disable import/extensions */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-underscore-dangle */
+
+import { WebAudioModule } from '@webaudiomodules/sdk';
+import {ParamMgrFactory } from '@webaudiomodules/sdk-parammgr'
+import { h, render } from 'preact';
+import { getBaseUrl } from '../../shared/getBaseUrl';
+import { RainbowView } from './views/RainbowView';
+import RainbowNode from './Node';
+import styleRoot from "./views/RainbowView.scss"
+
+export default class RainbowModule extends WebAudioModule<RainbowNode> {
+	//@ts-ignore
+	_baseURL = getBaseUrl(new URL('.', __webpack_public_path__));
+
+	_descriptorUrl = `${this._baseURL}/descriptor.json`;
+	nonce: string | undefined;
+
+	async _loadDescriptor() {
+		const url = this._descriptorUrl;
+		if (!url) throw new TypeError('Descriptor not found');
+		const response = await fetch(url);
+		const descriptor = await response.json();
+		Object.assign(this._descriptor, descriptor);
+		
+		return descriptor
+	}
+
+	async initialize(state: any) {
+		await this._loadDescriptor();
+		return super.initialize(state);
+	}
+
+	async createAudioNode(initialState: any) {
+		const synthNode = new RainbowNode(this.audioContext as AudioContext, this._baseURL);
+
+		await synthNode.loadRNBO()
+
+		const {internalParamsConfig, paramsConfig} = synthNode.paramMgrConfig()
+
+		const optionsIn = { internalParamsConfig, paramsConfig };
+		const paramMgrNode = await ParamMgrFactory.create<string, string>(this, optionsIn);
+
+		synthNode.setup(paramMgrNode);
+
+		if (initialState) synthNode.setState(initialState);
+		return synthNode;
+    }
+
+	async createGui() {
+		const div = document.createElement('div');
+		// hack because h() is getting stripped for non-use despite it being what the JSX compiles to
+		h("div", {})
+
+		div.setAttribute("style", "height: 100%; width: 100%; display: flex; flex: 1;")
+		div.setAttribute("width", "320")
+		div.setAttribute("height", "240")
+
+		var shadow = div.attachShadow({mode: 'open'});
+
+		if (this.nonce) {
+			// we've already rendered before, unuse the styles before using them again
+			this.nonce = undefined
+
+			//@ts-ignore
+			styleRoot.unuse()
+		}
+
+		this.nonce = Math.random().toString(16).substr(2, 8);
+		div.setAttribute("data-nonce", this.nonce)
+
+		// @ts-ignore
+		styleRoot.use({ target: shadow });
+		
+		render(<RainbowView plugin={this}></RainbowView>, shadow);
+
+		return div;
+	}
+
+	destroyGui(el: Element) {
+		if (el.getAttribute("data-nonce") == this.nonce) {
+			// this was the last time we rendered the GUI so clear the style
+			
+			//@ts-ignore
+			styleRoot.unuse()
+		}
+
+		render(null, el.shadowRoot)
+	}
+}
